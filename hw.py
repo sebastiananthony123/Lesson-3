67e33584c661595c31fc154b880e775415086102
sebastiananthony123 <tm@solumre.com>
2025-03-12 20:46:49 +0000

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,16 @@
+def mul():
+    a = int(input("Enter the radius for the area of the circle:"))
+    c=a*a*3.14
+ 
+    print(c)
+ 
+mul()
+
+   
+b = int(input("Enter the radius for the circumfrence of the circle:"))
+d=b*2*3.14
+
+print(d)
+
+mul()
+
